25b15ba8d08b5750aa75ffe96be47b5cb47bad5a
dgl3 <danigibert24@gmail.com>
2014-12-01 20:32:30 +0100

    Constants file

---
+++ src/cat/urv/imas/constants/AgentNames.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cat.urv.imas.constants;
+
+/**
+ *
+ * @author dgl3
+ */
+public class AgentNames {
+    public static final String ambulance = "ambulance";
+    public static final String fireman = "fireman";
+    public static final String hospital = "hospital";
+    
+}
